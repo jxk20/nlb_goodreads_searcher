c902a4a1806218ff67bb15783ac2a0f3bfd82e93
jxk20 <kohjingxuan@gmail.com>
2022-03-14 23:55:30 +0800

    add babel polyfill

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "@babel/polyfill";
 
 if (process.env.NODE_ENV === "production") {
   console.log = () => {};
