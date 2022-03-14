7350e883938e8f208e81487b74e29cd2759f51b3
jxk20 <kohjingxuan@gmail.com>
2022-03-15 00:22:09 +0800

    Edit sample test to pass

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Sample test", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Instructions/i);
   expect(linkElement).toBeInTheDocument();
 });
